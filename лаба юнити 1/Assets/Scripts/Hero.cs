740cf1b4d2e1a301d93dbfc18175268613b68f2b
sad-ukey <146202627+sad-ukey@users.noreply.github.com>
2024-11-11 23:51:09 +0300

    создал пол

--- лаба юнити 1/Assets/Scripts/Hero.cs
+++ лаба юнити 1/Assets/Scripts/Hero.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Hero : MonoBehaviour
+public class HeroController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f; // Скорость движения персонажа
+    public float jumpForce = 5f; // Сила прыжка
+
+    private bool isGrounded; // Проверка, находится ли персонаж на земле
+    private Rigidbody2D rb; // Ссылка на компонент Rigidbody2D
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); // Получаем компонент Rigidbody2D
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        float moveInput = Input.GetAxis("Horizontal"); // Получаем ввода по оси X
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y); // Изменяем скорость по X
+
+        // Поворот спрайта в зависимости от направления движения
+        if (moveInput > 0) 
+        {
+            transform.localScale = new Vector3(1, 1, 1); // Поворачиваемся вправо
+        }
+        else if (moveInput < 0) 
+        {
+            transform.localScale = new Vector3(-1, 1, 1); // Поворачиваемся влево
+        }
+    }
+
+    void Jump()
+    {
+        // Проверяем нажатие кнопки "Jump" и если персонаж на земле
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); // Применяем силу прыжка
+            isGrounded = false; // Персонаж теперь не на земле
+        }
+    }
+
+    // Метод для проверки, находится ли персонаж на земле
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) // Убедитесь, что объект земли имеет тег "Ground"
+        {
+            isGrounded = true; // Персонаж на земле
+        }
     }
 }
